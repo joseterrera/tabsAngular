c0c9b13c0bc29ee64bd392f08667526b9d23e4f0
joseterrera <joseterrera@yahoo.com>
2015-08-25 14:00:11 -0500

    activate and deactivate tabs

---
+++ app.js
@@ -0,0 +1,52 @@
+;(function(window) {
+
+angular.module('app', [])
+	.directive('tab', function(){
+		return {
+			//directive will be an element
+			restrict: 'E',
+			//to include a document inside another document
+			transclude: true,
+			//whenever the element directive is used, this is inserted into the DOM.
+			template: '<div role="tabpanel" ng-show="active" ng-transclude></div>',
+			require: '^tabset',
+			scope: {
+				heading: '@'
+			},
+			//linking function is similar to a controller
+			link: function(scope, elem, attr, tabsetCtrl){
+				scope.active = false
+				tabsetCtrl.addTab(scope)
+			}
+		}
+	})
+	.directive('tabset', function(){
+		return {
+			restrict: 'E',
+			transclude: true,
+			scope: {},
+			templateUrl: 'tabset.html',
+			bindToController: true,
+			//controllerAs allows us to bind properties directly to the controller object using this and have them accessible via tabset in the template
+			controllerAs: 'tabset',
+			//allows us to specify an angular controller
+			controller: function(){
+				var self = this
+				self.tabs = []
+				//this method will append a tab to the self.tabs array.
+				self.addTab = function addTab(tab){
+					self.tabs.push(tab)
+				}
+				self.select = function(selectedTab){
+					angular.forEach(self.tabs, function(tab){
+						if(tab.active && tab !==selectedTab){
+							tab.active =false;
+						}
+					})
+					selectedTab.active = true;
+				}
+			}
+		}
+	})
+
+})(window);
